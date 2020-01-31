f92a9e95cd13468cf22c0ce71d9d326848d28e57
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-01-31 22:03:32 +0200

    Terminal coloring at windows

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main() {
     HANDLE  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     int i;
-    for (i = 0; i < 255; ++i) {
-        SetConsoleTextAttribute(hConsole, i);
+    for (i = 0; i < 2; ++i) {
+        SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_RED);
         std::cout << i << " In color" << std::endl;
     }
     return 0;
