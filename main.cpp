1f1a644d26f0e569957131e92386342858666a8c
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-02-01 12:21:52 +0200

    Console application with PDCurses

--- main.cpp
+++ main.cpp
@@ -18,20 +18,20 @@
 #define WINDOW_COLOR_NO 20
 #define WINDOW_COLOR COLOR_PAIR(WINDOW_COLOR_NO)
 
-using namespace std;
+#define PROMPT_COLOR_NO 30
+#define PROMPT_COLOR COLOR_PAIR(PROMPT_COLOR_NO)
 
-void showSimpleWin();
+using namespace std;
 
 void init_wins(WINDOW **wins, int n);
 
-void win_show(WINDOW *win, char *label, int label_color);
+void wshow(WINDOW *win, char *label, int label_color);
 
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
+void wtitle(WINDOW *win, char *title, chtype color);
 
 void show_win_my();
 
 int main() {
-//    showSimpleWin();
     show_win_my();
     return 0;
 }
@@ -51,7 +51,8 @@ void show_win_my() {
     init_pair(1, COLOR_RED, COLOR_BLUE);
     init_pair(2, COLOR_GREEN, COLOR_BLUE);
     init_pair(3, COLOR_YELLOW, COLOR_BLUE);
-    init_pair(4, COLOR_WHITE, COLOR_BLACK);
+
+    init_pair(PROMPT_COLOR_NO, COLOR_WHITE, COLOR_BLACK);
 
     init_pair(BORDER_COLOR_NO, COLOR_WHITE, COLOR_BLUE);
     init_pair(WINDOW_COLOR_NO, COLOR_WHITE, COLOR_BLUE);
@@ -68,9 +69,9 @@ void show_win_my() {
 
     update_panels();
 
-    attron(COLOR_PAIR(4));
+    attron(PROMPT_COLOR);
     mvprintw(LINES - 2, 0, "Use tab to browse through the windows (F1 to Exit)");
-    attroff(COLOR_PAIR(4));
+    attroff(PROMPT_COLOR);
     doupdate();
 
     top = my_panels[2];
@@ -97,7 +98,7 @@ void init_wins(WINDOW **wins, int n) {
     for (i = 0; i < n; ++i) {
         wins[i] = newwin(NLINES, NCOLS, y, x);
         sprintf(label, "Window Number %d", i + 1);
-        win_show(wins[i], label, i + 1);
+        wshow(wins[i], label, i + 1);
         y += 3;
         x += 7;
     }
@@ -109,69 +110,20 @@ void wborder(WINDOW *win) {
             BOX_D_TL_CORNER, BOX_D_TR_CORNER, BOX_D_BL_CORNER, BOX_D_BR_CORNER);
 }
 
-void win_show(WINDOW *win, char *label, int label_color) {
+void wshow(WINDOW *win, char *title, int label_color) {
     wbkgd(win, WINDOW_COLOR);
+
     wattron(win, BORDER_COLOR);
     wborder(win);
     wattroff(win, BORDER_COLOR);
 
-    int width = getmaxx(win);
-    print_in_middle(win, 0, 0, width, label, COLOR_PAIR(label_color));
-}
-
-void print_in_middle(WINDOW *win, int startY, int startX, int width, char *string, chtype color) {
-    int length, x, y;
-    float temp;
-
-    if (win == NULL) {
-        win = stdscr;
-    }
-
-    getyx(win, y, x);
-    if (startX != 0) {
-        x = startX;
-    }
-
-    if (startY != 0) {
-        y = startY;
-    }
-
-    if (width == 0) {
-        width = 80;
-    }
-
-    length = strlen(string);
-    temp = (width - length) / 2;
-    x = startX + (int) temp;
-
-    wattron(win, color);
-    mvwprintw(win, y, x, "%s", string);
-    wattroff(win, color);
+    wtitle(win, title, COLOR_PAIR(label_color));
 
     refresh();
 }
 
-void showSimpleWin() {
-    initscr();
-    cbreak();
-    noecho();
-
-    const int max = 5;
-
-    int lines = 10, cols = 40, y = 2, x = 4, i;
-    WINDOW *my_wins[max];
-    PANEL *my_panels[max];
-
-    for (i = 0; i < max; ++i) {
-        my_wins[i] = newwin(lines, cols, y + 1 * i, x + 5 * i);
-        box(my_wins[i], 0, 0);
-
-        my_panels[i] = new_panel(my_wins[i]);
-    }
-
-    update_panels();
-    doupdate();
-
-    getch();
-    endwin();
+void wtitle(WINDOW *win, char *title, chtype color) {
+    wattron(win, color);
+    mvwprintw(win, 0, 2, "%s", title);
+    wattroff(win, color);
 }
